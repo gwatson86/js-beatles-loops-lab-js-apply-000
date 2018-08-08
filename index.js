d6cfa8968df892a8b569ed3b8a531cdbd534bbd7
Graham Watson <gwatson86@gmail.com>
2018-08-08 23:36:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 function theBeatlesPlay(musicians, instruments) {
   var beatlesArray = [];
   for (var i = 0; i < 5; i++) {
