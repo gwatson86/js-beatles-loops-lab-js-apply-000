a599f11c74e7c8f1f86dee48a577698b8f7b6d38
Graham Watson <gwatson86@gmail.com>
2018-08-08 23:34:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatlesArray = [];
-  for (i = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) {
     console.log(musicians(i) + " plays " + instruments(i))
   }
 } // add solution here
