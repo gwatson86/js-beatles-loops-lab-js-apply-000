c34f838e95d0065a36871a143252fd089dae0f02
Graham Watson <gwatson86@gmail.com>
2018-08-08 23:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatlesArray = [];
+  for (i = 0; i < 5; i++) {
+    console.log(musicians(i) + " plays " + instruments(i))
+  }
+} // add solution here
